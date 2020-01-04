e1ef3aab946f496a7034dde3d42cb0374a9780f2
Wuu <coolwuu0207@gmail.com>
2020-01-04 10:37:20 +0800

    Extract Method

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,18 +1,108 @@
-﻿using System;
+﻿using Dapper;
+using SlackAPI;
+using System;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
-using System.Threading;
-using Dapper;
-using SlackAPI;
 
 namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
         public bool Verify(string accountId, string password, string otp)
+        {
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+            var isLocked = GetAccountIsLocked(accountId, httpClient);
+            if (isLocked)
+            {
+                throw new FailedTooManyTimesException() { AccountId = accountId };
+            }
+
+            var dbPassword = GetPasswordFromDb(accountId);
+            var hashedPassword = GetHashedPassword(password);
+            var currentOtp = GetCurrentOtp(accountId, httpClient);
+
+            if (hashedPassword == dbPassword && otp == currentOtp)
+            {
+                ResetFailedCount(accountId, httpClient);
+                return true;
+            }
+            else
+            {
+                AddFailedCount(accountId, httpClient);
+                LogFailedCount(accountId, httpClient);
+                Notify(accountId);
+                return false;
+            }
+        }
+
+        private static bool GetAccountIsLocked(string accountId, HttpClient httpClient)
+        {
+            var isLockedResponse = httpClient.PostAsJsonAsync("api/failedCounter/IsLocked", accountId).Result;
+            isLockedResponse.EnsureSuccessStatusCode();
+            var isLocked = isLockedResponse.Content.ReadAsAsync<bool>().Result;
+            return isLocked;
+        }
+
+        private static void Notify(string accountId)
+        {
+            var message = $"account:{accountId} try to login failed.";
+            var slackClient = new SlackClient("my api token");
+            slackClient.PostMessage(messageResponse => { }, "my channel", message, "my bot name");
+        }
+
+        private static void LogFailedCount(string accountId, HttpClient httpClient)
+        {
+            var failedCountResponse =
+                httpClient.PostAsJsonAsync("api/failedCounter/GetFailedCount", accountId).Result;
+            failedCountResponse.EnsureSuccessStatusCode();
+            var failedCount = failedCountResponse.Content.ReadAsAsync<int>().Result;
+
+            var logger = NLog.LogManager.GetCurrentClassLogger();
+            logger.Info($"accountId:{accountId} failed times:{failedCount}");
+        }
+
+        private static void AddFailedCount(string accountId, HttpClient httpClient)
+        {
+            var addFailedCountResponse = httpClient.PostAsJsonAsync("api/failedCounter/Add", accountId).Result;
+            addFailedCountResponse.EnsureSuccessStatusCode();
+        }
+
+        private static void ResetFailedCount(string accountId, HttpClient httpClient)
+        {
+            var resetResponse = httpClient.PostAsJsonAsync("api/failedCounter/Reset", accountId).Result;
+            resetResponse.EnsureSuccessStatusCode();
+        }
+
+        private static string GetCurrentOtp(string accountId, HttpClient httpClient)
+        {
+            var response = httpClient.PostAsJsonAsync("api/otps", accountId).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"web api error, accountId:{accountId}");
+            }
+
+            var currentOtp = response.Content.ReadAsAsync<string>().Result;
+            return currentOtp;
+        }
+
+        private static string GetHashedPassword(string password)
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hashStringBuilder = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+            foreach (var theByte in crypto)
+            {
+                hashStringBuilder.Append(theByte.ToString("x2"));
+            }
+
+            var hashedPassword = hashStringBuilder.ToString();
+            return hashedPassword;
+        }
+
+        private static string GetPasswordFromDb(string accountId)
         {
             string dbPassword;
             using (var connection = new SqlConnection("my connection string"))
@@ -21,33 +111,12 @@ namespace DependencyInjectionWorkshop.Models
                     commandType: CommandType.StoredProcedure).SingleOrDefault();
             }
 
-            var crypt = new System.Security.Cryptography.SHA256Managed();
-            var hashStringBuilder = new StringBuilder();
-            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
-            foreach (var theByte in crypto)
-            {
-                hashStringBuilder.Append(theByte.ToString("x2"));
-            }
-            var hashedPassword = hashStringBuilder.ToString();
-
-            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
-            var response = httpClient.PostAsJsonAsync("api/otps", accountId).Result;
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception($"web api error, accountId:{accountId}");
-            }
-
-            var currentOtp = response.Content.ReadAsAsync<string>().Result;
-
-            if (hashedPassword == dbPassword && otp == currentOtp)
-            {
-                return true;
-            }
-
-            var message = $"account:{accountId} try to login failed.";
-            var slackClient = new SlackClient("my api token");
-            slackClient.PostMessage(response1 => { }, "my channel", message, "my bot name");
-            return false;
+            return dbPassword;
         }
+    }
+
+    public class FailedTooManyTimesException : Exception
+    {
+        public string AccountId { get; set; }
     }
 }
